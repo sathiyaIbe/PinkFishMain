c1b8705b7718d7b3b1e27891a79c81c775b92a2b
sathiyaibe <sathiyaibd@gmail.com>
2023-10-17 01:44:18 +0530

    fixed the bg_video

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -200,7 +200,7 @@ Pinkfish Live
   <div className=' md:hidden mb-11'>
   <section className="hero_continer flex flex-col justify-center  overflow-hidden "  >
 
-      <video src="/bg_video_1.mp4" ref={(el)=>firstRef=el} className='  h-[100%]' id='videos' autoplay loop playsinline muted ></video>
+      <video src="/bg_video_1.mp4" ref={(el)=>firstRef=el} className='  h-[100%]' id='videos' autoPlay loop playsInline muted ></video>
     
           <div className='flex flex-col justify-center h-full absolute self-center gap-3 md:mb-3 mb-24  mt-24 '>
         <img className=' w-3/5  self-center '  data-value="-30"    src='/main_logo.png'/>
