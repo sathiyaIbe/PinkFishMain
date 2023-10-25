b08e2cdee16843245a24847e4d477ea042285485
sathiyaibe <sathiyaibd@gmail.com>
2023-10-25 18:56:49 +0530

    added the script tag

--- app/layout.js
+++ app/layout.js
@@ -5,20 +5,20 @@ import '../styles/globals.css';
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
-     <Script
-  strategy="lazyOnload"
-  src={`https://www.googletagmanager.com/gtag/js?id=GTM-K6TW6MF`}
-/>
-<Script id="ga-script" strategy="lazyOnload">
-  {`
+     <script
+  async
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}>
+  </script>
+<script id="ga-script" strategy="lazyOnload" dangerouslySetInnerHTML={ { __html:`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'GTM-K6TW6MF', {
+    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
       page_path: window.location.pathname,
     });
-        `}
-</Script>
+        `}}>
+ 
+</script>
 
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
 
  experimental:{appDir: true},
-
+ env: {
+    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
+  }
 };
  
 module.exports = nextConfig;
