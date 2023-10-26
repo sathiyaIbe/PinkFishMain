247d4d9f161a7ed51ec14af17b870fabe73796c6
sathiyaibe <sathiyaibd@gmail.com>
2023-10-26 12:07:02 +0530

    fixing the error in production

--- next.config.js
+++ next.config.js
@@ -6,18 +6,7 @@ const nextConfig = {
  env: {
     GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
   },
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      dns: false,
-      child_process: false,
-      tls: false,
-    };
 
-    return config;
-  },
 
 };
  
