f922e4118640953007e3461bf4aa1a57ecae658b
sathiyaibe <sathiyaibd@gmail.com>
2023-10-26 12:02:31 +0530

    changed the environmental variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,13 @@ module.exports = {
       },
     },
   },
+
   plugins: [
-    require('flowbite/plugin')
-  ],
+    require('flowbite/plugin'),
+    require('postcss-import'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('autoprefixer'),
+    require('tailwindcss'),
+  ]
+
 };
