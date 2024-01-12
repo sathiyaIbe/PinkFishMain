b8c38ff7cbc81ff0dbcc8137ded9ffea70f92a69
sathiyaibe <sathiyaibd@gmail.com>
2024-01-12 20:37:43 +0530

    added new gallery for countdown

--- app/Gallery/page.jsx
+++ app/Gallery/page.jsx
@@ -9,7 +9,7 @@ import { Footer, NewNavbar } from '../../components';
 
 
 const Gallery = () => {
-  const [toogle,setToogle]=useState(true)
+  const [toogle,setToogle]=useState('1')
   return (
     <>
     <NewNavbar />
@@ -21,11 +21,88 @@ const Gallery = () => {
     </div>
 
     <div className= ' flex gap-3 self-center'>
-      <button className={toogle?'pink_btn':"gallery_btn" } onClick={()=>{setToogle(true)}}><img src='/gallery_logo_1.png'/> </button>
-      <button className={toogle?'gallery_btn':"pink_btn" } onClick={()=>{setToogle(false)}}><img src='/gallery_logo_2.png'/> </button>
+    <button className={toogle==='1'?'pink_btn':"gallery_btn" } onClick={()=>{setToogle('1')}}><img src='/gallery_logo_3.png'/> </button>
+      <button className={toogle==='2'?'pink_btn':"gallery_btn" } onClick={()=>{setToogle('2')}}><img src='/gallery_logo_1.png'/> </button>
+      <button className={toogle==='3'?'pink_btn':"gallery_btn" } onClick={()=>{setToogle('3')}}><img src='/gallery_logo_2.png'/> </button>
 
     </div>
-    {toogle?
+    {toogle==='1'?
+    <div className='self-center flex flex-col  gap-4 md:w-fit w-[80%]'>
+      <div className='flex gap-4'>
+        <div className='image_11 '>
+          <div className='flex'>
+        <img className='galler_svg_1' src='Gallery/svg_1.svg'/>
+        </div>
+        <img src='Gallery/new_gallery_countdown_1.png'  alt='gallery'/>
+        </div>
+        <div className='image_11 '>
+        <img src='Gallery/new_gallery_countdown_2.png'className="" alt='gallery'/>
+        </div>
+       
+      </div>
+      <div className='flex gap-4'>
+      <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_3.png' alt='gallery'/>
+        </div>
+        <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_4.png' alt='gallery'/>
+        </div>
+        <div className='flex'>
+        <img className='galler_svg_4' src='Gallery/svg_3.svg'/>
+        </div>
+      </div>
+      <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_5.png' alt='gallery'/>
+        <div className='flex'>
+        <img className='galler_svg_2' src='Gallery/svg_5.svg'/>
+        </div>
+        </div>
+      <div className='flex gap-4'>
+      <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_6.png' alt='gallery'/>
+        </div>
+        <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_7.png' alt='gallery'/>
+        </div>
+       
+      </div>
+      <div className='flex gap-4'>
+      <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_8.png' alt='gallery'/>
+        </div>
+        <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_9.png' alt='gallery'/>
+        </div>
+        <div className='flex'>
+        <img className='galler_svg_5' src='Gallery/svg_2.svg'/>
+        </div>
+        <div className='flex'>
+        <img className='galler_svg_3' src='Gallery/svg_4.svg'/>
+        </div>
+      </div>
+      <div className='flex  gap-4'>
+      <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_10.png' alt='gallery'/>
+        </div>
+       
+
+      <div className='flex flex-col gap-4'>
+      
+      <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_11.png' alt='gallery'/>
+        </div>
+        
+        <div className='image_11'>
+        <img src='Gallery/new_gallery_countdown_12.png' alt='gallery'/>
+        </div>
+        <div className='flex'>
+        <img className='galler_svg_5' src='Gallery/svg_6.svg'/>
+        </div>
+        </div>
+      </div>
+    </div>
+:<></>}
+    {toogle==='2'?
     <div className='self-center flex flex-col  gap-4 md:w-fit w-[80%]'>
       <div className='flex gap-4'>
         <div className='image_11'>
@@ -99,7 +176,10 @@ const Gallery = () => {
         </div>
         </div>
       </div>
-    </div>:
+    </div>
+:<></>}
+{toogle==='3'?
+
     <div className='self-center flex flex-col  gap-4 md:w-fit w-[80%]'>
     <div className='flex gap-4'>
       <div className='image_11'>
@@ -169,12 +249,12 @@ const Gallery = () => {
       <img src='Gallery/new_gallery_23.png' alt='gallery'/>
       </div>
       <div className='flex'>
-      <img className='galler_svg_5' src='Gallery/svg_24.svg'/>
+      <img className='galler_svg_5' src='Gallery/svg_6.svg'/>
       </div>
       </div>
     </div>
   </div>
-}
+:<></>}
 {/* <div className='hidden md:block'>
   <img className='galler_svg_1' src='Gallery/svg_1.svg'/>
   <img className='galler_svg_2' src='Gallery/svg_5.svg'/>
